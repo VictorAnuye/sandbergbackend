d23e505eec33b4a9c7b45c0e11962acce3105bb4
victoranuye <victoranuye@gmail.com>
2026-01-08 22:45:36 +0100

    Describe your update

--- src/controllers/notificationController.js
+++ src/controllers/notificationController.js
@@ -11,3 +11,16 @@ export const getNotifications = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// controllers/notificationController.js
+import Booking from "../models/booking.js"
+
+export const getNotificationCount = async (req, res) => {
+  try {
+    // Count bookings that are "Reserved" (pending check-in)
+    const count = await Booking.countDocuments({ status: "Reserved" })
+    res.status(200).json({ count })
+  } catch (err) {
+    res.status(500).json({ message: "Server error" })
+  }
+}
