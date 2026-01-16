96b4d10db4aa1efb2bb595aa1eb675a010f14998
victoranuye <victoranuye@gmail.com>
2026-01-16 17:20:34 +0100

    Describe your update

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { login, createAdmin, createReceptionist, logout,forgotPassword,validateResetCode,resetPassword,} from "../controllers/authController.js";
 import { protect, authorize } from "../middleware/authMiddleware.js";
 import User from "../models/user.js";
-import { getCurrentUser } from "../controllers/authController.js";
+import { getCurrentUser, getOnlineReceptionists } from "../controllers/authController.js";
 import crypto from "crypto";
 import bcrypt from "bcryptjs";
 import { sendEmail } from "../utils/sendEmail.js";
@@ -17,7 +17,8 @@ router.post("/logout", protect, logout);
 router.post("/forgot-password", forgotPassword);
 router.post("/validate-reset-code", validateResetCode);
 router.post("/reset-password", resetPassword);
-router.get("/me", protect, getCurrentUser)
+router.get("/me", protect, getCurrentUser);
+router.get("/online", protect, getOnlineReceptionists);
 // In authRoutes.js
 router.post("/logout", protect, async (req, res) => { 
   req.user.currentToken = null;
