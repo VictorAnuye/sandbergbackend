064b675968bca6e3ce22b5b84fb0533fa1f85489
victoranuye <victoranuye@gmail.com>
2026-01-25 12:33:23 +0100

    Describe your update

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -3,14 +3,20 @@ import nodemailer from "nodemailer";
 export const sendEmail = async (to, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
-      host: process.env.SMTP_HOST,
-      port: Number(process.env.SMTP_PORT),
-      secure: false, // MUST be false for port 587
-      auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-      },
-    });
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // MUST be false for 587
+  auth: {
+    user: "apikey",
+    pass: process.env.SMTP_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false, // important on Render
+  },
+  connectionTimeout: 10000,
+  greetingTimeout: 10000,
+  socketTimeout: 10000,
+});
 
     await transporter.sendMail({
       from: process.env.EMAIL_FROM,
